6e007a9cc561bf6122819aba79a2b603353a95cb
Bert Tijskens <engelbert.tijskens@uantwerpen.be>
2023-02-09 09:35:21 +0100

    "Initial commit from `micc2 create wetppr`"

---
+++ wetppr/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+"""
+Package wetppr
+=======================================
+
+Top-level package for wetppr.
+"""
+
+__version__ = "0.0.0"
+
+
+def hello(who='world'):
+    """'Hello world' method.
+
+    :param str who: whom to say hello to
+    :returns: a string
+    """
+    result = "Hello " + who
+    return result
+
+# Your code here...
