f7ed4ec32009932c4bf0736905007ac184553736
Bert Tijskens <engelbert.tijskens@uantwerpen.be>
2023-10-06 11:43:20 +0200

    added scripts/mac/test_numba_omp.py

---
+++ scripts/mac/test-numba-omp.py
@@ -0,0 +1,15 @@
+import numba
+numba.config.THREADING_LAYER='omp'
+numba.config.NUMBA_NUM_THREADS=2
+print(f"{numba.config.THREADING_LAYER=}")
+print(f"{numba.config.NUMBA_NUM_THREADS=}")
+
+@numba.jit(nopython=True, parallel=True)
+def omp():
+    # numba.
+    for i in numba.prange(2):
+        print(f"[i={i}] thread {numba.get_thread_id()}/{numba.get_num_threads()}")
+    
+
+if __name__ == "__main__":
+    omp()
