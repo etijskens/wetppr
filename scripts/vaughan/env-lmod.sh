bb6753f9d5812cab2bb4644d6c7305fe4beff560
the-john-doe <john.doe@example.com>
2023-10-25 08:31:10 +0200

    improve script scripts/vaughan/env-lmod.sh
    Changes to be committed:
    	modified:   scripts/vaughan/env-lmod.sh

--- scripts/vaughan/env-lmod.sh
+++ scripts/vaughan/env-lmod.sh
@@ -1,22 +1,89 @@
 #!/bin/bash
+# call this script as
+#   > source path/to/wetppr/scripts/vaughan/env-lmod.sh foss
+# to load the foss toolchain_name, or 
+#   > source path/to/wetppr/scripts/vaughan/env-lmod.sh intel
+# to load the intel toolchain_name.
 
-if [ "$1" = "" ]
-then 
-    echo "ERROR: toolchain not provided: [foss|intel]" 1>&2
-    return 1
+# Debug this script ------------------------------------------------------------
+# set -eux
+
+# Functions --------------------------------------------------------------------
+usage()
+{
+    echo "Load usefull modules from a given toolchain (default is ${default_toolchain_name}/${default_toolchain_version})"
+    echo "Usage: "
+    echo "  $0 -h : help"
+    echo "  $0 [toolchain_name[/toolchain_version]]"
+    exit 2
+}
+
+ml_verbose()
+{
+    echo "ml $1"
+    ml $1
+}
+
+# Set default values -----------------------------------------------------------
+default_toolchain_name=foss
+default_toolchain_version=2022a
+
+# Handle command line arguments ------------------------------------------------
+while getopts "vh" opt; do
+  case $opt in
+    h)  
+        echo "usage"
+        usage
+        ;;
+  esac
+done
+shift $((OPTIND-1))
+if [ $@ ]
+then
+    supplied_toolchain=$1
+    # Did the user also supply also supply a toolchain version
+    supplied_toolchain_version=$(basename ${supplied_toolchain})
+    if [ "${supplied_toolchain_version}" = "${supplied_toolchain}" ]
+    then # no toolchain version supplied, only the toolchain name
+        toolchain_name=${supplied_toolchain}
+        toolchain_version=${default_toolchain_version}
+    else # both toolchain name and version supplied
+        toolchain_name=$(dirname ${supplied_toolchain})
+        toolchain_version=${supplied_toolchain_version}
+    fi
+else # nothing provided
+    toolchain_name=${default_toolchain_name}
+    toolchain_version=${default_toolchain_version}
 fi
 
-export toolchain_version=2022a
+echo
+echo "Using toolchain_name ${toolchain_name}/${toolchain_version} "
+echo
 
-export toolchain=$1
-
+# Set LMOD environmemt ---------------------------------------------------------
 module --force purge
-ml calcua/${toolchain_version}
-ml $toolchain
-ml SciPy-bundle
-ml numba
-ml CMake
-ml
+
+ml_verbose calcua/${toolchain_version}
+ml_verbose $toolchain_name
+ml_verbose SciPy-bundle
+ml_verbose numba
+ml_verbose CMake
 echo
-echo "Using toolchain ${toolchain}/${toolchain_version} "
-echo
+
+# Allow to pip install missing python packages yourself ------------------------
+# If $PYTHONUSERBASE is already set, it is kept as is.
+# Otherwise, it is set to ${VSC_DATA}/.local.
+if [ -z "$PYTHONUSERBASE" ]
+then
+    export PYTHONUSERBASE=${VSC_DATA}/.local
+fi
+# Make sure the folder exists:
+mkdir -p ${PYTHONUSERBASE}
+# check if $PATH has ${PYTHONUSERBASE}/bin
+if [[ ":$PATH:" == *":${PYTHONUSERBASE}/bin:"* ]]
+then
+    : # PATH is correctly set
+else
+    export PATH="$PATH:${PYTHONUSERBASE}/bin"
+fi
+# ------------------------------------------------------------------------------
