e4d8b0a533584b3d913fee6a7ba2ac967e9f5270
the-john-doe <john.doe@example.com>
2023-10-26 09:42:25 +0200

    Annotate scripts/vaughan/hello-mpi4py/hello-mpi4py.py

--- scripts/vaughan/hello-mpi4py/hello-mpi4py.py
+++ scripts/vaughan/hello-mpi4py/hello-mpi4py.py
@@ -1,4 +1,3 @@
-import itertools
 import os
 jobid  = os.getenv('SLURM_JOB_ID')
 stepid = os.getenv('SLURM_STEP_ID')
@@ -10,16 +9,20 @@ comm = MPI.COMM_WORLD
 
 
 def print_environment_variable(name):
+    """Print the name and the value of an environment value."""
     value = os.getenv(name)
     print(f"${{{name}}}={value}")
 
 
 if __name__ == '__main__':
-    # print some environment variables
+    # Print some environment variables (only if the MPI rank is 0)
     if comm.rank == 0:
         print_environment_variable('SLURM_NTASKS')
         print_environment_variable('SLURM_CPUS_PER_TASK')
 
+    # Print a line with the job-id, job-step-id, MPI rank, total number of MPI
+    # processes, process id, and the affinity (the id of the CPU on which 
+    # the MPI process is running). Every MPI process will print one line.
     print(f'jobid.stepid={jobid}.{stepid}, MPI_rank={comm.rank}/{comm.size}, {pid=} : affinity={affinity}')
 
     
